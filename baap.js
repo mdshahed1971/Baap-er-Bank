74e9cf08620cd0240f679d491f30f7019d70aa28
shahed <mdshahed19711@gmail.com>
2025-10-28 15:30:54 +0600

    added baap.js

---
+++ baap.js
@@ -0,0 +1,20 @@
+document.getElementById('btn-submit').addEventListener('click', function(event){
+    event.preventDefault();
+    const userEmail = document.getElementById('user-email');
+    const userPassword = document.getElementById('user-password');
+    const userPasswordValue = userPassword.value;
+    const convertedUserPassword = parseInt(userPasswordValue);
+
+    if(userEmail){
+        userEmail.value = 'mdshahed19711@gmail.com';
+        if(convertedUserPassword===1234){
+            window.location.href= 'inside.html'
+        }
+        else{
+            alert('Invalid pin number')
+        }
+    }
+    else{
+        alert('Invalid email')
+    }
+})
